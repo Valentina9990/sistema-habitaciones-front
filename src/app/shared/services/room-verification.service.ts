1a360989f309c1fe8ffbea7e094465ac4dd89b56
Valentina9990 <dania.v.sarmiento@gmail.com>
2025-05-25 18:18:58 -0500

    refactor: integrar endpoint para traer habitaciones sin verificar

--- src/app/shared/services/room-verification.service.ts
+++ src/app/shared/services/room-verification.service.ts
@@ -1,10 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { forkJoin, map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environments';
 import { Room } from '../models/room';
 import { VerificationHistoryItem } from '../models/verification-item';
-import { VerificationRequest } from '../models/verification-request';
 
 @Injectable({
   providedIn: 'root'
@@ -15,15 +14,14 @@ export class RoomVerificationService {
   constructor(private http: HttpClient) { }
 
   getUnverifiedRooms(): Observable<Room[]> {
-    return this.http.get<Room[]>(this.API_URL_ROOMS);
+    return this.http.get<Room[]>(`${this.API_URL_ROOMS}/search?verificada=true`);
   }
 
   getVerificationHistory(): Observable<VerificationHistoryItem[]> {
     return this.http.get<VerificationHistoryItem[]>(`${this.API_URL_ROOMS}/history`);
   }
 
-  verifyRoom(verificationData: VerificationRequest): Observable<any> {
-    return this.http.post(`${this.API_URL_ROOMS}`, verificationData);
+  verifyRoom(id: number, room: Partial<Room>): Observable<Room> {
+    return this.http.put<Room>(`${this.API_URL_ROOMS}/${id}`, room);
   }
-  
 }
