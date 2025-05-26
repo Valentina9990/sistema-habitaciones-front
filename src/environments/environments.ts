a6237fc86567f834449d2b598d3b04203dc1e36d
Valentina9990 <dania.v.sarmiento@gmail.com>
2025-05-26 01:03:39 -0500

    api reserva

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
   api: {
-    users: 'http://localhost:8081/api/auth',
-    rooms: 'http://localhost:8083/api/rooms',
+    users: 'http://localhost:8086/api/auth',
+    rooms: 'http://localhost:8081/api/rooms',
     reviews: 'http://localhost:8080/microResenas/reviews',
-    booking: 'http://localhost:8080/microRservas/reservas',
+    booking: 'http://localhost:8084/api/reservas',
   },
 };
